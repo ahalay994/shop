716e33de5a3c23f37e35d43b4efaec5d12c29cb5
Nikolay <ahalay994@gmail.com>
2020-11-22 00:15:47 +0300

    Create user migrations

--- code/database/migrations/2020_11_21_194500_create_users_table.php
+++ code/database/migrations/2020_11_21_194500_create_users_table.php
@@ -15,12 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('role_id')->unsigned();
             $table->string('name');
+            $table->string('login');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('active')->default(true);
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
